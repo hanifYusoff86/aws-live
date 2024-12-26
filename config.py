4fd61c76ab2d1b434feb3f14fbf2183834445836
hanifYusoff86 <hanifUMK2@gmail.com>
2024-12-26 13:18:38 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cfr5yspebunc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "hanif-employee"
+customregion = "us-east-1"
+
